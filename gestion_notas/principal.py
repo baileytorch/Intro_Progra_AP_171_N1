69b8cb79953288e798337df25457064b591fe990
baileytorch <baileytorch@hotmail.com>
2025-05-29 15:57:17 -0400

    actualización gestión asignaturas

--- gestion_notas/principal.py
+++ gestion_notas/principal.py
@@ -10,5 +10,17 @@ def obtener_listado_asignaturas():
         print(f'[{contador}] {asignatura}')
 
 def obtener_asignatura_individual():
-    busqueda = input("Ingrese Asignarura a buscar: ")
-    busqueda = input("Ingrese Asignarura a")
+    asignatura_encontrada = 'asignatura NO encontrada'
+    busqueda = input("Ingrese asignatura a buscar: ")
+    for asignatura in asignaturas:
+        if busqueda.lower() in asignatura.lower():
+            asignatura_encontrada = asignatura
+    return asignatura_encontrada
+
+def guardar_nueva_asignatura():
+    obtener_listado_asignaturas()
+    nueva_asignatura = input('Ingrese nueva asignatura: ')
+    asignaturas.append(nueva_asignatura.title())
+    obtener_listado_asignaturas()
+
+guardar_nueva_asignatura()
