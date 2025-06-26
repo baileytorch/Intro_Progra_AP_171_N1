f509e2cc9a9067db9138def99cf74c972a90306c
baileytorch <baileytorch@hotmail.com>
2025-06-26 16:00:33 -0400

    Actualizando acceso a DB

---
+++ README.md
@@ -0,0 +1,3 @@
+Ejemplo de normalización de DB
+
+https://learn.microsoft.com/es-es/office/troubleshoot/access/database-normalization-description
---
+++ gestion_notas/data/DDL.sql
@@ -0,0 +1,28 @@
+CREATE TABLE docentes(
+    id_docente INT AUTO_INCREMENT,
+    rut_docente INT NOT NULL UNIQUE,
+    digito_ver VARCHAR(1) NOT NULL,
+    nombre_docente VARCHAR(250) NOT NULL,
+    email_docente VARCHAR(250) NULL,
+    
+    CONSTRAINT pk_docentes PRIMARY KEY (id_docente)
+);
+
+CREATE TABLE asignaturas(
+    id_asignatura INT AUTO_INCREMENT,
+    codigo_asig VARCHAR(6) NOT NULL UNIQUE,
+    nombre_asig VARCHAR(250) NOT NULL,
+    descripcion_asig VARCHAR(250) NULL,
+    
+    CONSTRAINT pk_asignaturas PRIMARY KEY (id_asignatura)
+);
+
+CREATE TABLE docentes_asignaturas(
+    id_docente_asignatura INT AUTO_INCREMENT,
+    id_tabla_docentes INT NOT NULL,
+    id_tabla_asignaturas INT NOT NULL,
+    
+    CONSTRAINT pk_docentes_asignaturas PRIMARY KEY (id_docente_asignatura),
+    CONSTRAINT fk_tabla_docentes FOREIGN KEY (id_tabla_docentes) REFERENCES docentes (id_docente),
+    CONSTRAINT fk_tabla_asignaturas FOREIGN KEY (id_tabla_asignaturas) REFERENCES asignaturas (id_asignatura)
+);
